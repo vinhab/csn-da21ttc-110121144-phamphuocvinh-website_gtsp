92c5580168d18e9c62ffda0420681f24d3a23833
vinhab <vinhne2603@gmail.com>
2023-11-25 21:46:42 +0700

    Cập nhật code

---
+++ src/DoAnCoSo/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.EnterpriseServices.Internal;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//Khai bao
+using DoAnCoSo.Models;
+
+namespace DoAnCoSo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var dbContext = new QUANLYWEBEntities();
+                var data = dbContext.Database.SqlQuery<SANPHAM>("SELECT * FROM SANPHAM").ToList();
+                return View(data);
+        }
+    }
+
+}
